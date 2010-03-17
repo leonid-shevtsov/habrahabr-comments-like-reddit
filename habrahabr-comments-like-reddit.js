7db2e47ff4eedcce0ffab9002fef48c6e6c7c84b
Leonid Shevtsov <leonid@shevtsov.me>
2010-03-17 10:12:00 +0200

    make script work on company blogs

--- habrahabr-comments-like-reddit.js
+++ habrahabr-comments-like-reddit.js
@@ -2,9 +2,10 @@
 // @name           Habrahabr 'comments like reddit' mod
 // @description    Makes Habrahabr comments behave like Reddit's: no avatars, order by comment rating, collapsible threads, author's comments marked with [S]; Tested with Firefox and Chrome
 // @author         Leonid Shevtsov
-// @version        0.1
+// @version        0.2
 // @namespace      http://leonid.shevtsov.me
 // @include        http://habrahabr.ru/blogs/*
+// @include        http://habrahabr.ru/company/*
 // @include        http://*.habrahabr.ru/blog/*
 // ==/UserScript==
 //
